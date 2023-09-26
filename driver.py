d0a5632b4624b09915b3854d6dede5e5f211610c
arpytanshu <arpytanshu@gmail.com>
2023-09-27 01:20:24 +0530

    checkpoint watermarker.

--- driver.py
+++ driver.py
@@ -2,29 +2,31 @@
 import torch
 from time import perf_counter
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from watermark import WaterMarking, WaterMarkingConfig
 
 torch.set_default_device('cuda')
 
 tick = perf_counter()
 model = AutoModelForCausalLM.from_pretrained("microsoft/phi-1_5", trust_remote_code=True, torch_dtype="auto")
 tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-1_5", trust_remote_code=True, torch_dtype="auto")
-
-
 elapsed = perf_counter() - tick
 print(f"{elapsed=:.4f}")
 
+
+wm_cfg = WaterMarkingConfig(vocab_size=tokenizer.vocab_size,
+
+watermarker = WaterMarking(wm_cfg)
+
+
 #%%
 
-inputs = tokenizer('''```python
+
+input_ids = tokenizer('''```python
 def print_prime(n):
    """
    Print all primes between 1 and n
-   """''', return_tensors="pt", return_attention_mask=False)
+   """''', return_tensors="pt", return_attention_mask=False)['input_ids']
 
-
-outputs = model.generate(**inputs, max_length=200)
-text = tokenizer.batch_decode(outputs)[0]
-print(text)
-
+output = model(input_ids, output_hidden_state=True)
 
 
---
+++ watermark.py
@@ -0,0 +1,83 @@
+
+import torch
+from typing import Callable
+
+
+class WaterMarkingConfig:
+    def __init__(self, 
+                 vocab_size: int, 
+                 gamma: float = 0.5, 
+                 hardness: float = 1.5, 
+                 device: torch.device = torch.device('cpu'), 
+                 soft_mode: bool = True):
+        self.vocab_size = vocab_size
+        self.gamma = gamma
+        self.hardness = hardness # delta
+        self.soft_mode = soft_mode
+        self.device = device
+
+        # Calculate G and R list sizes
+        self.G_list_size = int(self.vocab_size * self.gamma)
+        self.R_list_size = self.vocab_size - self.G_list_size
+
+    def __repr__(self):
+        return \
+         f"WaterMarkingConfig(vocab_size={self.vocab_size}, \
+         gamma={self.gamma}, \
+         hardness={self.hardness}, \
+         soft_mode={self.soft_mode})"
+    
+
+class WaterMarking:
+   def __init__(self, cfg: WaterMarkingConfig, hash_fn: Callable):
+      self.cfg = cfg
+      self.hash_fn = hash_fn
+   
+   def _hash_input(self, input_ids):
+      '''
+      input_ids: expected_shape is Bx1
+      step 3 of Algorithm 2, which creates R list & G list.
+      '''
+      g = torch.Generator()
+      bch_sz = input_ids.shape[0]
+      G_list = torch.empty((bch_sz, self.cfg.G_list_size), dtype=torch.long)
+      R_list = torch.empty((bch_sz, self.cfg.R_list_size), dtype=torch.long)
+      
+      for ix, id in enumerate(input_ids):
+         g.manual_seed(self.hash_fn(id.item()))
+         rand_perm = torch.randperm(n=self.cfg.vocab_size, generator=g)
+         G_list[ix] = rand_perm[:self.cfg.G_list_size]
+         R_list[ix] = rand_perm[self.cfg.G_list_size:]
+      
+      return {'R_list': R_list, 'G_list': G_list}
+   
+   def _hard_mode(self, logits, lists):
+      logits.scatter_(index=lists['R_list'], dim=1, value=0)
+   
+   def _soft_mode(self, logits, lists):
+      index = lists['G_list']
+      src = torch.ones(index.shape) * self.cfg.hardness
+      logits.scatter_reduce_(dim=1, index=index, src=src, reduce='sum')
+
+   def __call__(self, logits: torch.FloatTensor, input_ids: torch.LongTensor, return_lists:bool = False):
+      assert logits.dim() == 3, \
+         f"Expected logits to have dimension 3, but got dimension {logits.dim()}"
+      assert input_ids.dim() == 2, \
+         f"Expected input_ids to have dimension 2, but got dimension {input_ids.dim()}"
+
+      last_input_ids = input_ids[:, -1].view(-1, 1) # slice input_ids to be hashed. [b]
+      lists = self._hash_input(last_input_ids)
+
+      logits = logits[:, -1, :] # slice logits from last input token. [b x v]
+      
+      if self.cfg.soft_mode:
+         self._soft_mode(logits, lists)
+      else:
+         self._hard_mode(logits, lists)
+
+      if return_lists:
+         return logits, lists
+      else:
+         return logits
+      
+
